cce9fa5d3bc6b7c01c3d5e91a06a9e407b12a09d
ymk88877 <951688403@qq.com>
2025-04-10 08:47:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,13 +10,13 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-echo "src-git customsd https://github.com/Ljzkirito/smartdns-openwrt.git;Release46-conf" >> "feeds.conf.default"
+#echo "src-git customsd https://github.com/Ljzkirito/smartdns-openwrt.git;Release46-conf" >> "feeds.conf.default"
 
 # 取消Dnsmasq缓存，由smartdns负责。不修改ttl
-sed -i 's/cachesize\t8000/cachesize\t0/g' package/network/services/dnsmasq/files/dhcp.conf
-sed -i 's/mini_ttl\t3600/mini_ttl\t0/g' package/network/services/dnsmasq/files/dhcp.conf
+#sed -i 's/cachesize\t8000/cachesize\t0/g' package/network/services/dnsmasq/files/dhcp.conf
+#sed -i 's/mini_ttl\t3600/mini_ttl\t0/g' package/network/services/dnsmasq/files/dhcp.conf
 
 # Remove 6in4
-sed -i 's/ +6in4//g' package/emortal/ipv6-helper/Makefile
-sed -i '/hotplug.d/d' package/emortal/ipv6-helper/Makefile
-rm -fv package/emortal/ipv6-helper/files/60-6in4
+#sed -i 's/ +6in4//g' package/emortal/ipv6-helper/Makefile
+#sed -i '/hotplug.d/d' package/emortal/ipv6-helper/Makefile
+#rm -fv package/emortal/ipv6-helper/files/60-6in4
